592225574c054328493c59ffbadac7077acb1cc9
Aidan Curley <aidan@Rampage.Home>
2021-12-19 15:46:00 +0000

    Update html files; ;

--- flaskr/app.py
+++ flaskr/app.py
@@ -4,44 +4,133 @@ from flask_mysqldb import MySQL
 from flask_session import Session
 from tempfile import mkdtemp
 
+from werkzeug.security import check_password_hash, generate_password_hash
 
-app=Flask(__name__)
+from flaskr.classes import Account
+from flaskr.helper import apology
+
+
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem instead of cookies
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'sth_fruity'
+
 Session(app)
+mysql = MySQL(app)
 
 
-
-@app.route("/")
-def home():
-    return redirect('/register')
-
-@app.route('/login', methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    """Login with username and password"""
-    global CURRENT_USER
-    # Forget any stored or cached user_ids
+    """Log user in"""
+
+    # Forget any user_id
     session.clear()
-    return render_template('auth/login.html')
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username and password:
+            return apology("incomplete details", 403)
+
+        # Query database for student_id
+        cmd = "SELECT * FROM account WHERE username =%s"
+        results = db_select(cmd, [username])
+
+        if len(results) != 1:
+            return apology("invalid details", 403)
+
+        account = Account(results[0])
+        if check_password_hash(password, account.hash):
+            return apology("incorrect password", 403)
+
+        # Remember which user has logged in
+        session["account_id"] = account.id
+
+        return render_template("account.html", account=account)
+
+    else:
+        return render_template('auth/login.html')
 
 
-@app.route('/register')
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register for a new account"""
-    return render_template('auth/register.html')
+    """Register user"""
+    if request.method == "GET":
+        # Display form for user to create account
+        return render_template("auth/register.html")
+    else:
+        new_username = request.form.get("username")
+        new_password = generate_password_hash(request.form.get("password"))
+        new_f_name = request.form.get("f_name")
+        new_surname = request.form.get("surname")
+        new_email = request.form.get("email")
+
+        if new_username == '' or new_password == '' or new_f_name == '' or new_surname == '' or new_email == '':
+            return apology('must complete all fields', 403)
+
+        new_account = Account(new_username, new_password, new_f_name, new_surname, new_email)
+
+        # check password and confirmation are same
+        if request.form.get("password") != request.form.get("confirm-password"):
+            return apology("password and confirmation do not match", 403)
+
+        # check username is unique
+        if db_select("SELECT * FROM account WHERE username = %s", [new_username]):
+            return apology("you already have an account", 403)
+
+        # create row in db
+        db_commit("INSERT INTO account(username, hash, f_name, surname, email) VALUES (%s, %s, %s, %s, %s)",
+                  new_account.get_details())
+
+        # make sure that the new user is logged in
+        results = db_select("SELECT * FROM account WHERE username = %s", [new_account.username])
+
+        # # set the session, so we know who is logged-in
+        session["account_id"] = results[0][0]
+        return render_template("account.html", account=new_account)
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/account", methods=["GET", "POST"])
+def home(account):
+    if request.method == "GET":
+        return render_template('account.html', account=account)
 
 
 @app.route('/logout')
 def logout():
     """Log user out"""
-    global CURRENT_USER
     # Clear the current user's details
     session.clear()
     # Redirect user to login form
-    return redirect('auth/login')
-#
-#if __name__ == "__main__":
+    return redirect("/login")
+
+
+def db_select(cmd, params):
+    cursor = mysql.connection.cursor()
+    cursor.execute(cmd, params)
+    results = cursor.fetchall()
+    cursor.close()
+    return results
+
+
+def db_commit(cmd, params):
+    cursor = mysql.connection.cursor()
+    cursor.execute(cmd, params)
+    mysql.connection.commit()
+    cursor.close()
+    return
+
+# if __name__ == "__main__":
 #    app.run(host='0.0.0.0', debug=True)
