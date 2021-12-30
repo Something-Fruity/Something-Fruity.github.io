d362318e0c9ca0a729ad60e1b80a728b4433653b
AidanCurley <aidanpcurley@gmail.com>
2021-12-30 15:31:22 +0000

    Refactor login and register to include last login details

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,15 +1,16 @@
-
-from flask import Flask, render_template, session, request, redirect, flash
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, redirect, flash, get_flashed_messages
 from flask_session import Session
 from tempfile import mkdtemp
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.classes import Account
-from flaskr.helper import apology
-from flaskr.database_helper import db_select, db_commit
+from flaskr.models.base import Session
+from flaskr.models.account import Account
+from flaskr.models.player import Player
+from flaskr.models.persona import Persona
+from flaskr.models.game import Game
 
+from datetime import date
 
 app = Flask(__name__)
 
@@ -23,41 +24,32 @@ app.config["SESSION_TYPE"] = "filesystem"
 
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
-app.config["MYSQL_HOST"] = "172.19.0.2"
-app.config["MYSQL_PORT"] = 3306
+app.config["MYSQL_HOST"] = "127.0.0.1"
 app.config["MYSQL_DB"] = "sth_fruity"
-mysql = MySQL(app)
-Session(app)
+
+app.secret_key = 'super secret key'
+
+session = Session()
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if not username and password:
-            return apology("incomplete details", 403)
 
         # Query database for student_id
-        cmd = "SELECT * FROM account WHERE username =%s"
-        results = db_select(mysql, cmd, [username])
+        account = session.query(Account).filter_by(username=username).first()
 
-        if len(results) != 1:
-            return apology("invalid details", 403)
-
-        account = Account(results[0])
-        if check_password_hash(password, account.hash):
-            return apology("incorrect password", 403)
-
-        # Remember which user has logged in
-        session["account_id"] = account.id
-
-        return render_template("account.html", account=account)
+        if account and account.check_password(password):
+            flash(f'Welcome, {account.username}.', 'alert-success')
+            session.query(Account).filter_by(username=username).update({'last_login': date.today()})
+            session.commit()
+            return render_template("account.html", account=account)
+        else:
+            flash("INCORRECT DETAILS: Please check your details and try again.", 'alert-danger')
+            return render_template('auth/login.html')
 
     else:
         return render_template('auth/login.html')
@@ -77,49 +69,35 @@ def register():
         new_email = request.form.get("email")
 
         if new_username == '' or new_password == '' or new_f_name == '' or new_surname == '' or new_email == '':
-            return apology('must complete all fields', 403)
+            flash("You must complete all fields", 'alert-danger')
+            return render_template('auth/register.html')
 
-        new_account = Account(new_username, new_password, new_f_name, new_surname, new_email)
+        # check username is unique
+        if session.query(Account).filter_by(username=new_username).first():
+            flash("There is already an account associated with this username", 'alert-danger')
+            return render_template('auth/register.html')
 
         # check password and confirmation are same
         if request.form.get("password") != request.form.get("confirm-password"):
-            return apology("password and confirmation do not match", 403)
+            flash("Your password and confirmation do not match", 'alert-danger')
+            return render_template('auth/register.html')
 
-        # check username is unique
-        if db_select(mysql, "SELECT * FROM account WHERE username = %s", [new_username]):
-            return apology("you already have an account", 403)
+        new_account = Account(new_username, new_password, new_f_name, new_surname, new_email, date.today())
+        session.add(new_account)
+        session.commit()
 
-        # create row in db
-        db_commit(mysql, "INSERT INTO account(username, hash, f_name, surname, email) VALUES (%s, %s, %s, %s, %s)",
-                  new_account.get_details())
-
-        # make sure that the new user is logged in
-        results = db_select(mysql, "SELECT * FROM account WHERE username = %s", [new_account.username])
-
-        # # set the session, so we know who is logged-in
-        session["account_id"] = results[0][0]
+        # # make sure that the new user is logged in
         return render_template("account.html", account=new_account)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if "account_id" in session:
-        results = db_select(mysql, "SELECT * FROM account WHERE id = %s", "2")
-        accounts = Account(results[0])
-        return render_template('game.html', account=accounts)
-    else:
-        return render_template('auth/login.html')
-    return render_template('game.html')
+    return redirect('/account')
 
 
 @app.route("/account", methods=["GET", "POST"])
 def home():
-    if "account_id" in session:
-        results = db_select(mysql, "SELECT * FROM account WHERE id = %s", "2")
-        accounts = Account(results[0])
-        return render_template('account.html', account=accounts)
-    else:
-        return render_template('auth/login.html')
+    return render_template('account.html', account=None)
 
 
 @app.route('/logout')
@@ -131,13 +109,4 @@ def logout():
     return redirect("/login")
 
 
-
-    
-@app.route('/users')
-def show_users():
-    print(session)
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM account''')
-    rv = cur.fetchall()
-    return str(rv)
 
