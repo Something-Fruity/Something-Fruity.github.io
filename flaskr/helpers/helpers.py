4a7659e5417ea622af4d219c848df5a6e362622f
AidanCurley <aidanpcurley@gmail.com>
2021-12-30 15:30:31 +0000

    Remove unused apology

--- flaskr/helpers/helpers.py
+++ flaskr/helpers/helpers.py
@@ -1,24 +1,5 @@
 import re
-from flask import render_template
-
 from flaskr.constants import EMAIL_REGEX
-
-
-def apology(message, code=400):
-    """Render message as an apology to user.
-    """
-
-    def escape(s):
-        """
-        Escape special characters.
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-
-    return render_template("errors/apology.html", top=code, bottom=escape(message)), code
 
 
 def is_valid_email(email):
