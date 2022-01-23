a972721865b728f6ea7b67b4506a178ebc254b16
Anrich Potgieter <anrichp@gmail.com>
2022-01-23 21:45:29 +0000

    upfate the database connect string to refer to the docker database contrainer

--- flaskr/models/base.py
+++ flaskr/models/base.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-db = create_engine('mysql://root:root@127.0.0.1/sth_fruity')
+db = create_engine('mysql://root:root@mysql-db/sth_fruity')
 Session = sessionmaker(bind=db)
 
 Base = declarative_base()
