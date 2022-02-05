71e416fa58a1c848490471abd5cc98bab4ef851c
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-02-05 14:42:16 +0000

    create_engine back to original

--- flaskr/models/base.py
+++ flaskr/models/base.py
@@ -4,10 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db = create_engine('mysql://root:root@172.19.0.2/sth_fruity')
+#below commented out by Lukasz, do not remove it please
+#db = create_engine('mysql://root:root@172.19.0.2/sth_fruity')
 #db = create_engine('mysql://root:root@127.0.0.0/sth_fruity')
-#below commented out by Lukasz
-#db = create_engine('mysql://root:root@mysql-db/sth_fruity')
+
+db = create_engine('mysql://root:root@mysql-db/sth_fruity')
 Session = sessionmaker(bind=db)
 
 Base = declarative_base()
