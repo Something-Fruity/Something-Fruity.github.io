d13cffda38f60b0953b3d030c89b2ae27f03e499
lucosmo <lukasz.kosmaczewski@gmail.com>
2022-02-13 22:30:58 +0000

    bootcreate()

--- flaskr/game/templates/game.js
+++ flaskr/game/templates/game.js
@@ -43,3 +43,38 @@ var config = {
         disableWebAudio: true
     }
 };
+
+var game = new Phaser.Game(config);
+var cursors;
+var gameCase = 0;
+var gameCoins = 0;
+var pointsText;
+function bootLoader ()
+{
+    this.load.image('btnStart', 'assets/images/start.png');
+    this.load.image('apple', 'assets/images/apple.png');
+    this.load.image('banana', 'assets/images/banana.png');
+    this.load.image('berry', 'assets/images/berry.png');
+    this.load.image('berrya', 'assets/images/pear.png');
+    this.load.image('bomb', 'assets/images/warm.png');
+    this.load.image('background_image', 'assets/images/bj.jpg');
+    this.load.image('player_role', 'assets/images/ren.png');
+    this.load.image('blackboard', 'assets/images/blackboard.png');
+}
+
+function bootCreate ()
+{
+    this.bg = this.add.image(0, 0, 'background_image').setOrigin(0);
+    this.bg.width = game.config.width;
+    this.bg.height = game.config.height;
+    this.bg.displayWidth = game.config.width;
+    this.bg.displayHeight = game.config.height;
+
+    this.btnStart = this.add.image(0,0,'btnStart').setOrigin(0).setInteractive();
+    Phaser.Display.Align.In.Center(this.btnStart,this.bg);
+
+    this.btnStart.on("pointerdown",function(pointer){
+        gameCase = 1;
+        stateStart('demo',this)
+    },this);
+}
